1d1c3d6767d703b5663ee90f3e3c27513b01532d
kadeem-lewis <email@kadeemlewis.com>
2024-10-09 19:20:01 -0400

    began creating commands for app

---
+++ app/commands/greet/__init__.py
@@ -0,0 +1,10 @@
+"""Command to greet the user"""
+
+from app.commands import Command
+
+
+class GreetCommand(Command):
+    """Command to greet the user"""
+
+    def execute(self):
+        print("Hello, world!")
