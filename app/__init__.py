7d02d1ffc76ddb7802690fac3b057d9fae7ec9e2
kadeem-lewis <email@kadeemlewis.com>
2024-10-09 22:58:09 -0400

    updated plugins to be loaded on app start and to also be stored for both index position and name

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,6 @@
 import pkgutil
 import importlib
 from app.commands import CommandHandler, Command
-from app.commands.greet import GreetCommand
-from app.commands.menu import MenuCommand
-from app.commands.exit import ExitCommand
-from app.commands.github import GithubCommand
-from app.commands.goodbye import GoodbyeCommand
 
 
 class App:
@@ -19,8 +14,8 @@ class App:
     def load_plugins(self):
         """Dynamically load all plugins in the plugins directory"""
         plugins_package = "app.plugins"
-        for _, plugin_name, is_pkg in pkgutil.iter_modules(
-            [plugins_package.replace(".", "/")]
+        for index, (_, plugin_name, is_pkg) in enumerate(
+            pkgutil.iter_modules([plugins_package.replace(".", "/")]), start=1
         ):
             if is_pkg:  # Ensure it's a package
                 plugin_module = importlib.import_module(
@@ -32,13 +27,26 @@ class App:
                         if issubclass(
                             item, (Command)
                         ):  # Assuming a BaseCommand class exists
-                            self.command_handler.register_command(plugin_name, item())
+                            if plugin_name == "menu":
+                                self.command_handler.register_command(
+                                    plugin_name, item(self.command_handler)
+                                )
+                                self.command_handler.register_command(
+                                    str(index), item(self.command_handler)
+                                )
+                            else:
+                                self.command_handler.register_command(
+                                    plugin_name, item()
+                                )
+                                self.command_handler.register_command(
+                                    str(index), item()
+                                )
                     except TypeError:
                         continue  # If item is not a
 
     def start(self):
         """Start the application."""
-        # self.load_plugins()
+        self.load_plugins()
 
         print("Type 'exit' to exit.")
         while True:  # REPL Read, Evaluate, Print, Loop
