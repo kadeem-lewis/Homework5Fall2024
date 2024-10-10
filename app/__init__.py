8db8ee49e32f81d7b5576be7f1f3e7b7f6f9c41c
kadeem-lewis <email@kadeemlewis.com>
2024-10-09 21:48:08 -0400

    added a load plugins function to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 """Main application module."""
 
-from app.commands import CommandHandler
+import pkgutil
+import importlib
+from app.commands import CommandHandler, Command
 from app.commands.greet import GreetCommand
 from app.commands.menu import MenuCommand
 from app.commands.exit import ExitCommand
@@ -14,13 +16,29 @@ class App:
     def __init__(self):
         self.command_handler = CommandHandler()
 
+    def load_plugins(self):
+        """Dynamically load all plugins in the plugins directory"""
+        plugins_package = "app.plugins"
+        for _, plugin_name, is_pkg in pkgutil.iter_modules(
+            [plugins_package.replace(".", "/")]
+        ):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(
+                    f"{plugins_package}.{plugin_name}"
+                )
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(
+                            item, (Command)
+                        ):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a
+
     def start(self):
         """Start the application."""
-        self.command_handler.register_command("greet", GreetCommand())
-        self.command_handler.register_command("menu", MenuCommand(self.command_handler))
-        self.command_handler.register_command("exit", ExitCommand())
-        self.command_handler.register_command("github", GithubCommand())
-        self.command_handler.register_command("goodbye", GoodbyeCommand())
+        # self.load_plugins()
 
         print("Type 'exit' to exit.")
         while True:  # REPL Read, Evaluate, Print, Loop
