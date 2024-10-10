b68a3174c23e56046dcba804b678593296cb514f
kadeem-lewis <email@kadeemlewis.com>
2024-10-10 01:38:23 -0400

    created commands for add, subtract, multiply, divide

---
+++ app/plugins/add/__init__.py
@@ -0,0 +1,12 @@
+"""Add command plugin"""
+
+from app.commands import Command
+from app.calculator import Calculator
+
+
+class AddCommand(Command):
+    """Command to add two numbers"""
+
+    def execute(self, num_one, num_two):
+        """Add two numbers"""
+        return Calculator.add(num_one, num_two)
