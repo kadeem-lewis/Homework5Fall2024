3613c5c6f1875011f223f59c6c450fa7147b4e57
kadeem-lewis <email@kadeemlewis.com>
2024-10-09 20:01:12 -0400

    added additional commands

---
+++ app/commands/goodbye/__init__.py
@@ -0,0 +1,10 @@
+"""Command to say goodbye to the user"""
+
+from app.commands import Command
+
+
+class GoodbyeCommand(Command):
+    """Command to say goodbye to the user"""
+
+    def execute(self):
+        print("Goodbye, world!")
