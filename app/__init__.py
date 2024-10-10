e1e66ac057f5d5680e908f36e0a6efcfa93c4970
kadeem-lewis <email@kadeemlewis.com>
2024-10-09 20:59:56 -0400

    added additional commands to app

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from app.commands import CommandHandler
 from app.commands.greet import GreetCommand
 from app.commands.menu import MenuCommand
 from app.commands.exit import ExitCommand
+from app.commands.github import GithubCommand
+from app.commands.goodbye import GoodbyeCommand
 
 
 class App:
     """Main application class."""
 
-    def __init__(self):  # Constructor
+    def __init__(self):
         self.command_handler = CommandHandler()
 
     def start(self):
@@ -17,6 +19,8 @@ class App:
         self.command_handler.register_command("greet", GreetCommand())
         self.command_handler.register_command("menu", MenuCommand(self.command_handler))
         self.command_handler.register_command("exit", ExitCommand())
+        self.command_handler.register_command("github", GithubCommand())
+        self.command_handler.register_command("goodbye", GoodbyeCommand())
 
         print("Type 'exit' to exit.")
         while True:  # REPL Read, Evaluate, Print, Loop
